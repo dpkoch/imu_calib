3416e725dee1a841032e7e38afaca1eafa3fb18a
Daniel Koch <dkoch89@gmail.com>
2015-11-07 00:29:59 -0700

    Started on the linear algebra

--- include/accel_calib/accel_calib.h
+++ include/accel_calib/accel_calib.h
@@ -6,6 +6,8 @@
  */
 
 #include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
 
 #include <string>
 
@@ -16,7 +18,7 @@ class AccelCalib
 {
 public:
 
-  enum Orientation { XPOS, XNEG, YPOS, YNEG, ZPOS, ZNEG };
+  enum Orientation { XPOS = 0, XNEG, YPOS, YNEG, ZPOS, ZNEG };
 
   AccelCalib();
   AccelCalib(std::string calib_file);
@@ -30,13 +32,16 @@ public:
 
   // calibration procedure
   void beginCalib(int measurements, double reference_acceleration);
-  void addMeasurement(Orientation orientation, double ax, double ay, double az);
+  bool addMeasurement(Orientation orientation, double ax, double ay, double az);
   void computeCalib();
 
   // calibration application
   void applyCalib(double raw[3], double corrected[3]);
 
 protected:
+  static const int reference_index_[];
+  static const int reference_sign_[];
+
   bool calib_ready_;
 
   Eigen::Matrix3d SM_; //!< combined scale and misalignment parameters
@@ -44,8 +49,8 @@ protected:
 
   double reference_acceleration_; //!< expected acceleration measurement (e.g. 1.0 for unit of g's, 9.80665 for unit of m/s^2)
 
-  Eigen::MatrixXd meas_; //!< least squares measurements matrix
-  Eigen::VectorXd ref_; //!< least squares expected measurements vector
+  Eigen::SparseMatrix<double> meas_; //!< least squares measurements matrix
+  Eigen::SparseMatrix<double> ref_; //!< least squares expected measurements vector
   int num_measurements_; //!< number of measurements expected for this calibration
   int measurements_received_; //!< number of measurements received for this calibration
 };
--- src/accel_calib/accel_calib.cpp
+++ src/accel_calib/accel_calib.cpp
@@ -10,6 +10,9 @@
 namespace accel_calib
 {
 
+const int reference_index_[] = { 0, 0, 1, 1, 2, 2 };
+const int reference_sign_[] = { 1, -1, 1, -1, 1, -1 };
+
 AccelCalib::AccelCalib() : calib_ready_(false) {}
 
 AccelCalib::AccelCalib(std::string calib_file)
@@ -38,11 +41,35 @@ void AccelCalib::beginCalib(int measurements, double reference_acceleration)
   num_measurements_ = measurements;
   measurements_received_ = 0;
 
-  //TODO initialize matrices
+  meas_ = Eigen::SparseMatrix<double>(3*measurements, 12);
+  meas_.reserve(4*3*measurements);
+
+  ref_ = Eigen::SparseMatrix<double>(3*measurements, 1);
+  ref_.reserve(measurements);
 }
 
-void AccelCalib::addMeasurement(AccelCalib::Orientation orientation, double ax, double ay, double az)
+bool AccelCalib::addMeasurement(AccelCalib::Orientation orientation, double ax, double ay, double az)
 {
+  if (measurements_received_ < num_measurements_)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      meas_.insert(3*measurements_received_ + i, 3*i) = ax;
+      meas_.insert(3*measurements_received_ + i, 3*i + 1) = ay;
+      meas_.insert(3*measurements_received_ + i, 3*i + 2) = az;
+
+      meas_.insert(3*measurements_received_ + i, 9 + i) = 1.0;
+    }
+
+    ref_.insert(3*measurements_received_ + reference_index_[orientation], 0) = reference_sign_[orientation] *  reference_acceleration_;
+
+    measurements_received_++;
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 void AccelCalib::computeCalib()
